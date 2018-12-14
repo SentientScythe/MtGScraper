bdab931dd42e0d088507402f7459fc3ae5ad9248
Scott Lamb <scottlamb@dissolve.com>
2018-12-14 11:46:19 -0700

    Added layout for new program

---
+++ JS/agg_cards_layout.js
@@ -0,0 +1,22 @@
+// open connection
+// truncate 6 columns in cards
+// get list of decks
+// close connection
+
+// for each deck of decks
+	// open connection
+	// fetch deck.cards
+	// close connection
+
+	// for each card of deck.cards
+		// open connection
+		// accumulate number
+		
+		// if deck ranks 1
+			// accumulate rank 1 number
+
+		// case deck.format: legacy, vintage, modern, standard
+			// accumulate format numbers
+
+		// close connection
+// exit
