a767f2c22fc927c066f02e87c818c42274adc99a
Scott Lamb <scottlamb@attergangar.ca>
2018-12-15 16:51:11 -0700

    Update agg_cards_layout.js

--- JS/agg_cards_layout.js
+++ JS/agg_cards_layout.js
@@ -1,7 +1,28 @@
+// setup
+const Promise = require('bluebird');
+Promise.longStackTraces();
+
+const {
+	Pool
+} = require('pg');
+const pool = new Pool({
+		user: 'postgres',
+		database: 'mtg'
+	});
+
 // open connection
+const client = await pool.connect();
+
 // truncate 6 columns in cards
+const wipe_columns = '';
+await client.query(wipe_columns);
+
 // get list of decks
+const select_decks = 'SELECT cards, format, rank FROM mtg.tournament_decks WHERE unknown_cards_main = FALSE';
+const deck_urls = await client.query(select_decks);
+
 // close connection
+await client.release();
 
 // for each deck of decks
 	// open connection
