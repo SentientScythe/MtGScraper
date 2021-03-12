953e5c60ac20a5c92bf6688d20cf791933890cec
Scott Lamb <sentientscythe@gmail.com>
2021-03-12 13:17:39 -0700

    Update aggregation code

--- JS/agg_cards.js
+++ JS/agg_cards.js
@@ -1,99 +1,73 @@
+/*jshint esversion: 8 */
+
 const Promise = require('bluebird');
-Promise.longStackTraces();
+// Promise.longStackTraces(); DEBUG ONLY
 
-const {
-	Pool
-} = require('pg');
+// Invoke SQL
+const { Pool } = require('pg');
 const pool = new Pool({
-		user: 'postgres',
-		database: 'mtg'
-	});
+	user: 'postgres',
+	database: 'mtg'
+});
+// Invoke SQL
 
-let agg_cards = async() => {
-	const wipe_columns = 'UPDATE mtg.card_stats SET count_all = 0, count_1 = 0, count_legacy = 0, count_modern = 0, count_standard = 0, count_vintage = 0';
-	const select_decks = "SELECT cards, format, rank FROM mtg.tournament_decks tds LEFT JOIN mtg.deck_stats ds ON tds.deck_url = ds.deck_url WHERE date > '30-09-2010' AND format IN ('Legacy', 'Modern', 'Standard', 'Vintage') AND main = 60 AND unknown_cards_main = FALSE";
+const spaceReplace = / /g;
+const sReplace = /%s/g;
+const updateCount = 'UPDATE mtg.card_stats SET count_%s = count_%s + $1 WHERE name = $2';
+const updateAll = updateCount.replace(sReplace, 'all');
+const update1 = updateCount.replace(sReplace, '1');
 
+let aggregateCardStats = async () => {
 	const client = await pool.connect();
-	await client.query(wipe_columns);
-	const select_decks_response = await client.query(select_decks);
-	await client.release();
+	await client.query(
+		'UPDATE mtg.card_stats SET count_all = 0, count_1 = 0, count_legacy = 0, count_modern = 0, count_standard = 0, count_vintage = 0'
+	);
+	const decks = await client.query(
+		"SELECT cards, format, rank FROM mtg.tournament_decks tds LEFT JOIN mtg.deck_stats ds ON tds.deck_url = ds.deck_url WHERE date > '30-09-2010' AND unknown_cards_main = FALSE"
+	);
+	client.release();
 
-	const card_regex = /(SB:\s+)?(\d*)\s*(\[\w*\])?\s*(.+)/;
-	const count_all = 'UPDATE mtg.card_stats SET count_all = count_all + $1 WHERE name = $2';
-	const count_1 = 'UPDATE mtg.card_stats SET count_1 = count_1 + $1 WHERE name = $2';
-	const count_legacy = 'UPDATE mtg.card_stats SET count_legacy = count_legacy + $1 WHERE name = $2';
-	const count_modern = 'UPDATE mtg.card_stats SET count_modern = count_modern + $1 WHERE name = $2';
-	const count_standard = 'UPDATE mtg.card_stats SET count_standard = count_standard + $1 WHERE name = $2';
-	const count_vintage = 'UPDATE mtg.card_stats SET count_vintage = count_vintage + $1 WHERE name = $2';
+	for (const deck of decks.rows) {
+		const rankFirst = '1' === deck.rank;
+		const updateFormat = updateCount.replace(sReplace, deck.format.toLowerCase().replace(spaceReplace, '_'));
 
-	for (deck of select_decks_response.rows) {
-		for (card of deck.cards) {
-			const groups = await card_regex.exec(card);
+		for (const card of deck.cards) {
+			const groups = /(SB:\s+)?(\d*)\s*(\[\w*\])?\s*(.+)/.exec(card);
 			const sideboard = groups[1];
-			const number_of_cards = Number(groups[2]);
-			const card_name = groups[4];
-			const card_names = card_name.includes('/') ? card_name.split(' / ') : null;
-
+			const numberOfCards = Number(groups[2]);
+			const cardName = groups[4];
+			const cardNames = cardName.includes('/') ? cardName.split(' / ') : null;
 			const client = await pool.connect();
-			if (card_names) {
-				await client.query(count_all, [number_of_cards, card_names[0]]);
-				await client.query(count_all, [number_of_cards, card_names[1]]);
 
-				if (deck.rank == '1') {
-					await client.query(count_1, [number_of_cards, card_names[0]]);
-					await client.query(count_1, [number_of_cards, card_names[1]]);
+			if (cardNames) {
+				const cardData1 = [numberOfCards, cardNames[0]];
+				const cardData2 = [numberOfCards, cardNames[1]];
+				await client.query(updateAll, cardData1);
+				await client.query(updateAll, cardData2);
+
+				if (rankFirst) {
+					await client.query(update1, cardData1);
+					await client.query(update1, cardData2);
 				}
 
-				switch (deck.format) {
-				case 'Legacy':
-					await client.query(count_legacy, [number_of_cards, card_names[0]]);
-					await client.query(count_legacy, [number_of_cards, card_names[1]]);
-					break;
-
-				case 'Modern':
-					await client.query(count_modern, [number_of_cards, card_names[0]]);
-					await client.query(count_modern, [number_of_cards, card_names[1]]);
-					break;
-
-				case 'Standard':
-					await client.query(count_standard, [number_of_cards, card_names[0]]);
-					await client.query(count_standard, [number_of_cards, card_names[1]]);
-					break;
-
-				case 'Vintage':
-					await client.query(count_vintage, [number_of_cards, card_names[0]]);
-					await client.query(count_vintage, [number_of_cards, card_names[1]]);
-				}
+				await client.query(updateFormat, cardData1);
+				await client.query(updateFormat, cardData2);
 			} else {
-				await client.query(count_all, [number_of_cards, card_name]);
+				const cardData = [numberOfCards, cardName];
+				await client.query(updateAll, cardData);
 
-				if (deck.rank == '1') {
-					await client.query(count_1, [number_of_cards, card_name]);
+				if (rankFirst) {
+					await client.query(update1, cardData);
 				}
 
-				switch (deck.format) {
-				case 'Legacy':
-					await client.query(count_legacy, [number_of_cards, card_name]);
-					break;
-
-				case 'Modern':
-					await client.query(count_modern, [number_of_cards, card_name]);
-					break;
-
-				case 'Standard':
-					await client.query(count_standard, [number_of_cards, card_name]);
-					break;
-
-				case 'Vintage':
-					await client.query(count_vintage, [number_of_cards, card_name]);
-				}
+				await client.query(updateFormat, cardData);
 			}
 
-			await client.release();
+			client.release();
 		}
 	}
 
 	process.exit();
-}
+};
 
-agg_cards();
+aggregateCardStats();
--- JS/agg_stats.js
+++ JS/agg_stats.js
@@ -1,88 +1,52 @@
-// layouts
-const select_layouts = 'SELECT tds.deck_url, layouts stats FROM mtg.tournament_decks tds LEFT JOIN mtg.deck_stats ds ON tds.deck_url = ds.deck_url WHERE main = 60 AND unknown_cards_main = FALSE';
-
-// mana_costs
-const select_mana_costs = 'SELECT tds.deck_url, mana_costs stats FROM mtg.tournament_decks tds LEFT JOIN mtg.deck_stats ds ON tds.deck_url = ds.deck_url WHERE main = 60 AND unknown_cards_main = FALSE';
-
-// cmcs
-const select_cmcs = 'SELECT tds.deck_url, cmcs stats FROM mtg.tournament_decks tds LEFT JOIN mtg.deck_stats ds ON tds.deck_url = ds.deck_url WHERE main = 60 AND unknown_cards_main = FALSE';
-
-// colors
-const select_colors = 'SELECT tds.deck_url, colors stats FROM mtg.tournament_decks tds LEFT JOIN mtg.deck_stats ds ON tds.deck_url = ds.deck_url WHERE main = 60 AND unknown_cards_main = FALSE';
-
-// supertypes
-const select_supertypes = 'SELECT tds.deck_url, supertypes stats FROM mtg.tournament_decks tds LEFT JOIN mtg.deck_stats ds ON tds.deck_url = ds.deck_url WHERE main = 60 AND unknown_cards_main = FALSE';
-
-// types
-const select_types = 'SELECT tds.deck_url, types stats FROM mtg.tournament_decks tds LEFT JOIN mtg.deck_stats ds ON tds.deck_url = ds.deck_url WHERE main = 60 AND unknown_cards_main = FALSE';
-
-// subtypes
-const select_subtypes = 'SELECT tds.deck_url, subtypes stats FROM mtg.tournament_decks tds LEFT JOIN mtg.deck_stats ds ON tds.deck_url = ds.deck_url WHERE main = 60 AND unknown_cards_main = FALSE';
-
-// powers
-const select_powers = 'SELECT tds.deck_url, powers stats FROM mtg.tournament_decks tds LEFT JOIN mtg.deck_stats ds ON tds.deck_url = ds.deck_url WHERE main = 60 AND unknown_cards_main = FALSE';
-
-// toughnesses
-const select_toughnesses = 'SELECT tds.deck_url, toughnesses stats FROM mtg.tournament_decks tds LEFT JOIN mtg.deck_stats ds ON tds.deck_url = ds.deck_url WHERE main = 60 AND unknown_cards_main = FALSE';
+/*jshint esversion: 8 */
 
 const Promise = require('bluebird');
-Promise.longStackTraces();
+// Promise.longStackTraces(); DEBUG ONLY
 
-const {
-	Pool
-} = require('pg');
+// Invoke SQL
+const { Pool } = require('pg');
 const pool = new Pool({
-		user: 'postgres',
-		database: 'mtg'
-	});
+	user: 'postgres',
+	database: 'mtg'
+});
+// Invoke SQL
+
+// Invoke Log 4 JS
+const log4js = require('log4js');
+// Invoke Log 4 JS
 
 // for each category: find all possibilities and make them columns, then fill each table
-let agg_stats = async() => {
-	const path = 'StatCSVs/'; 
-	await do_the_work(path + 'layouts.csv', select_layouts);
-	await do_the_work(path + 'mana_costs.csv', select_mana_costs);
-	await do_the_work(path + 'cmcs.csv', select_cmcs);
-	await do_the_work(path + 'colors.csv', select_colors);
-	await do_the_work(path + 'supertypes.csv', select_supertypes);
-	await do_the_work(path + 'types.csv', select_types);
-	await do_the_work(path + 'subtypes.csv', select_subtypes);
-	await do_the_work(path + 'powers.csv', select_powers);
-	await do_the_work(path + 'toughnesses.csv', select_toughnesses);
-}
+let aggregateStats = async () => {
+	const stats = ['layouts', 'mana_costs', 'cmcs', 'colors', 'supertypes', 'types', 'subtypes', 'powers', 'toughnesses'];
+	for (const stat of stats) {
+		await doTheWork(
+			stat + '.tsv',
+			'SELECT tds.deck_url, %s stats FROM mtg.tournament_decks tds LEFT JOIN mtg.deck_stats ds ON tds.deck_url = ds.deck_url WHERE main = 60 AND unknown_cards_main = FALSE'.replace(
+				/%s/g,
+				stat
+			)
+		);
+	}
+};
 
-let do_the_work = async(filename, query) => {
-	const log4js = await require('log4js');
-	await log4js.configure({
-		appenders: {
-			out: {
-				type: 'file',
-				filename: filename,
-				layout: {
-					type: 'messagePassThrough'
-				}
-			}
-		},
-		categories: {
-		default: {
-				appenders: ['out'],
-				level: 'info'
-			}
-		}
+let doTheWork = async (filename, query) => {
+	log4js.configure({
+		appenders: { out: { type: 'file', filename: 'StatTSVs/' + filename, layout: { type: 'messagePassThrough' } } },
+		categories: { default: { appenders: ['out'], level: 'info' } }
 	});
-	const log = await log4js.getLogger('out');
+	const log = log4js.getLogger('out');
 	// Setup
 
 	// SQL
 	const client = await pool.connect();
-
 	const response = await client.query(query);
 	const response_rows = response.rows;
-
-	await client.release();
+	client.release();
 	// SQL
 
 	// Gather keys for headers
 	const keys = new Set();
+
 	for (const response_row of response_rows) {
 		for (const stat of response_row.stats) {
 			const key = stat[0];
@@ -92,7 +56,7 @@ let do_the_work = async(filename, query) => {
 
 	// Print headers
 	const headers = Array.from(keys.values()).join('\t');
-	await log.info('deck_url\t' + headers);
+	log.info('deck_url\t' + headers);
 
 	// Print stats
 	for (const response_row of response_rows) {
@@ -102,7 +66,7 @@ let do_the_work = async(filename, query) => {
 			var stat_value = null;
 
 			for (const stat of response_row.stats) {
-				if (key == stat[0]) {
+				if (key === stat[0]) {
 					stat_value = stat[1];
 					break;
 				}
@@ -115,8 +79,8 @@ let do_the_work = async(filename, query) => {
 			}
 		}
 
-		await log.info(entry);
+		log.info(entry);
 	}
-}
+};
 
-agg_stats();
+aggregateStats();
