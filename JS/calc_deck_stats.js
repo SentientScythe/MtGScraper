3dc19f4bb897dc40a168d3a432fa27c92b90fff9
Scott Lamb <sentientscythe@gmail.com>
2021-03-12 09:11:46 -0700

    Update calc_deck_stats

--- JS/calc_deck_stats.js
+++ JS/calc_deck_stats.js
@@ -1,45 +1,56 @@
+/*jshint esversion: 8 */
+
 const Promise = require('bluebird');
-const {
-	Client
-} = require('pg');
+// Promise.longStackTraces(); DEBUG ONLY
+
+// Invoke SQL
+const { Client } = require('pg');
 const client = new Client({
-		user: 'postgres',
-		database: 'mtg'
-	});
+	user: 'postgres',
+	database: 'mtg'
+});
+const cardClient = new Client({
+	user: 'postgres',
+	database: 'mtg2'
+});
+// Invoke SQL
 
-let calc_deck_stats = async() => {
+let calc_deck_stats = async () => {
 	await client.connect();
+	await cardClient.connect();
 
-	const select_decks = "SELECT deck_url FROM mtg.tournament_decks WHERE cards <> '{}' AND cards IS NOT NULL AND unknown_cards_main = FALSE";
+	const select_decks =
+		"SELECT deck_url FROM mtg.tournament_decks WHERE cards <> '{}' AND cards IS NOT NULL AND unknown_cards_main = FALSE";
 	const decks = await client.query(select_decks);
 
 	const select_cards = 'SELECT UNNEST(cards) FROM mtg.tournament_decks WHERE deck_url = $1';
 	const card_regex = /(SB:\s+)?(\d*)\s*(\[\w*\])?\s*(.+)/;
-	const select_card_data = 'SELECT * FROM mtg.cards WHERE name = $1';
+	const select_card_data = 'SELECT * FROM mtg2.cards WHERE name = $1';
 	const truncate_deck_stats = 'TRUNCATE TABLE mtg.deck_stats';
-	const insert_deck_stats = 'INSERT INTO mtg.deck_stats (deck_url, main, sideboard, layouts, mana_costs, cmcs, colors, supertypes, types, subtypes, powers, toughnesses) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)';
+	const insert_deck_stats =
+		'INSERT INTO mtg.deck_stats (deck_url, main, sideboard, layouts, mana_costs, cmcs, colors, supertypes, types, subtypes, powers, toughnesses) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)';
 	await client.query(truncate_deck_stats);
 
 	for (const deck of decks.rows) {
 		const deck_url = deck.deck_url;
 		const cards_response = await client.query(select_cards, [deck_url]);
-		var cards = cards_response.rows.map(r => r.unnest);
+		var cards = cards_response.rows.map((r) => r.unnest);
 		var stats = new Map([
-					['main', 0],
-					['sideboard', 0],
-					['layouts', new Map()],
-					['mana_costs', new Map()],
-					['cmcs', new Map()],
-					['colors', new Map()],
-					['supertypes', new Map()],
-					['types', new Map()],
-					['subtypes', new Map()],
-					['powers', new Map()],
-					['toughnesses', new Map()]
-				]);
+			['main', 0],
+			['sideboard', 0],
+			['layouts', new Map()],
+			['mana_costs', new Map()],
+			['cmcs', new Map()],
+			['colors', new Map()],
+			['supertypes', new Map()],
+			['types', new Map()],
+			['subtypes', new Map()],
+			['powers', new Map()],
+			['toughnesses', new Map()]
+		]);
 
 		for (const card of cards) {
-			const groups = await card_regex.exec(card);
+			const groups = card_regex.exec(card);
 			const sideboard = groups[1];
 			const number_of_cards = Number(groups[2]);
 			var card_name = groups[4];
@@ -49,10 +60,10 @@ let calc_deck_stats = async() => {
 
 			if (split_card) {
 				const card_names = card_name.split(' / ');
-				card_data = await client.query(select_card_data, [card_names[0]]);
-				split_card_data = await client.query(select_card_data, [card_names[1]]);
+				card_data = await cardClient.query(select_card_data, [card_names[0]]);
+				split_card_data = await cardClient.query(select_card_data, [card_names[1]]);
 			} else {
-				card_data = await client.query(select_card_data, [card_name]);
+				card_data = await cardClient.query(select_card_data, [card_name]);
 			}
 
 			if (!card_data.rows[0]) {
@@ -65,7 +76,7 @@ let calc_deck_stats = async() => {
 				stats.set('main', main_stat);
 
 				// Layouts
-				const layout = card_data.rows[0]['layout'];
+				const layout = card_data.rows[0].layout;
 				var layouts = stats.get('layouts');
 				const layout_stat = layouts.get(layout);
 				var new_layout_stat;
@@ -79,7 +90,7 @@ let calc_deck_stats = async() => {
 				layouts.set(layout, new_layout_stat);
 
 				// Mana Costs
-				const mana_cost = card_data.rows[0]['mana_cost'];
+				const mana_cost = card_data.rows[0].mana_cost;
 				var mana_costs = stats.get('mana_costs');
 				const mana_cost_stat = mana_costs.get(mana_cost);
 				var new_mana_cost_stat;
@@ -93,10 +104,9 @@ let calc_deck_stats = async() => {
 				mana_costs.set(mana_cost, new_mana_cost_stat);
 
 				if (split_card) {
-					const mana_cost = split_card_data.rows[0]['mana_cost'];
-					var mana_costs = stats.get('mana_costs');
+					const mana_cost = split_card_data.rows[0].mana_cost;
+					mana_costs = stats.get('mana_costs');
 					const mana_cost_stat = mana_costs.get(mana_cost);
-					var new_mana_cost_stat;
 
 					if (!mana_cost_stat) {
 						new_mana_cost_stat = number_of_cards;
@@ -108,7 +118,7 @@ let calc_deck_stats = async() => {
 				}
 
 				// CMCs
-				const cmc = card_data.rows[0]['cmc'];
+				const cmc = card_data.rows[0].cmc;
 				var cmcs = stats.get('cmcs');
 				const cmc_stat = cmcs.get(cmc);
 				var new_cmc_stat;
@@ -122,10 +132,9 @@ let calc_deck_stats = async() => {
 				cmcs.set(cmc, new_cmc_stat);
 
 				if (split_card) {
-					const cmc = split_card_data.rows[0]['cmc'];
-					var cmcs = stats.get('cmcs');
+					const cmc = split_card_data.rows[0].cmc;
+					cmcs = stats.get('cmcs');
 					const cmc_stat = cmcs.get(cmc);
-					var new_cmc_stat;
 
 					if (!cmc_stat) {
 						new_cmc_stat = number_of_cards;
@@ -137,11 +146,10 @@ let calc_deck_stats = async() => {
 				}
 
 				// Colors
-				const colors = card_data.rows[0]['colors'];
+				const colors = card_data.rows[0].colors;
+				var deck_colors = stats.get('colors');
 
 				if (colors) {
-					var deck_colors = stats.get('colors');
-
 					for (const color of colors) {
 						const color_stat = deck_colors.get(color);
 						var new_color_stat;
@@ -157,32 +165,29 @@ let calc_deck_stats = async() => {
 				}
 
 				if (split_card) {
-					const colors = split_card_data.rows[0]['colors'];
+					const colors = split_card_data.rows[0].colors;
 
 					if (colors) {
-						var deck_colors = stats.get('colors');
-
 						for (const color of colors) {
-							const color_stat = deck_colors.get(color);
-							var new_color_stat;
+							const color_stat = splitDeckColors.get(color);
+							var newSplitColorStat;
 
 							if (!color_stat) {
-								new_color_stat = number_of_cards;
+								newSplitColorStat = number_of_cards;
 							} else {
-								new_color_stat = color_stat + number_of_cards;
+								newSplitColorStat = color_stat + number_of_cards;
 							}
 
-							deck_colors.set(color, new_color_stat);
+							splitDeckColors.set(color, newSplitColorStat);
 						}
 					}
 				}
 
 				// Supertypes
-				const supertypes = card_data.rows[0]['supertypes'];
+				const supertypes = card_data.rows[0].supertypes;
+				var deck_supertypes = stats.get('supertypes');
 
 				if (supertypes) {
-					var deck_supertypes = stats.get('supertypes');
-
 					for (const supertype of supertypes) {
 						const supertype_stat = deck_supertypes.get(supertype);
 						var new_supertype_stat;
@@ -198,28 +203,26 @@ let calc_deck_stats = async() => {
 				}
 
 				if (split_card) {
-					const supertypes = split_card_data.rows[0]['supertypes'];
+					const supertypes = split_card_data.rows[0].supertypes;
 
 					if (supertypes) {
-						var deck_supertypes = stats.get('supertypes');
-
 						for (const supertype of supertypes) {
 							const supertype_stat = deck_supertypes.get(supertype);
-							var new_supertype_stat;
+							var newSplitSupertypeStat;
 
 							if (!supertype_stat) {
-								new_supertype_stat = number_of_cards;
+								newSplitSupertypeStat = number_of_cards;
 							} else {
-								new_supertype_stat = supertype_stat + number_of_cards;
+								newSplitSupertypeStat = supertype_stat + number_of_cards;
 							}
 
-							deck_supertypes.set(supertype, new_supertype_stat);
+							deck_supertypes.set(supertype, newSplitSupertypeStat);
 						}
 					}
 				}
 
 				// Types
-				const types = card_data.rows[0]['types'];
+				const types = card_data.rows[0].types;
 				var deck_types = stats.get('types');
 
 				for (const type of types) {
@@ -236,29 +239,27 @@ let calc_deck_stats = async() => {
 				}
 
 				if (split_card) {
-					const types = split_card_data.rows[0]['types'];
-					var deck_types = stats.get('types');
+					const types = split_card_data.rows[0].types;
 
 					for (const type of types) {
 						const type_stat = deck_types.get(type);
-						var new_type_stat;
+						var newSplitTypeStat;
 
 						if (!type_stat) {
-							new_type_stat = number_of_cards;
+							newSplitTypeStat = number_of_cards;
 						} else {
-							new_type_stat = type_stat + number_of_cards;
+							newSplitTypeStat = type_stat + number_of_cards;
 						}
 
-						deck_types.set(type, new_type_stat);
+						deck_types.set(type, newSplitTypeStat);
 					}
 				}
 
 				// Subtypes
-				const subtypes = card_data.rows[0]['subtypes'];
+				const subtypes = card_data.rows[0].subtypes;
+				var deck_subtypes = stats.get('subtypes');
 
 				if (subtypes) {
-					var deck_subtypes = stats.get('subtypes');
-
 					for (const subtype of subtypes) {
 						const subtype_stat = deck_subtypes.get(subtype);
 						var new_subtype_stat;
@@ -274,28 +275,26 @@ let calc_deck_stats = async() => {
 				}
 
 				if (split_card) {
-					const subtypes = split_card_data.rows[0]['subtypes'];
+					const subtypes = split_card_data.rows[0].subtypes;
 
 					if (subtypes) {
-						var deck_subtypes = stats.get('subtypes');
-
 						for (const subtype of subtypes) {
 							const subtype_stat = deck_subtypes.get(subtype);
-							var new_subtype_stat;
+							var newSplitSubtypeStat;
 
 							if (!subtype_stat) {
-								new_subtype_stat = number_of_cards;
+								newSplitSubtypeStat = number_of_cards;
 							} else {
-								new_subtype_stat = subtype_stat + number_of_cards;
+								newSplitSubtypeStat = subtype_stat + number_of_cards;
 							}
 
-							deck_subtypes.set(subtype, new_subtype_stat);
+							deck_subtypes.set(subtype, newSplitSubtypeStat);
 						}
 					}
 				}
 
 				// Powers
-				const power = card_data.rows[0]['power'];
+				const power = card_data.rows[0].power;
 				var powers = stats.get('powers');
 				const power_stat = powers.get(power);
 				var new_power_stat;
@@ -309,22 +308,21 @@ let calc_deck_stats = async() => {
 				powers.set(power, new_power_stat);
 
 				if (split_card) {
-					const power = split_card_data.rows[0]['power'];
-					var powers = stats.get('powers');
+					const power = split_card_data.rows[0].power;
 					const power_stat = powers.get(power);
-					var new_power_stat;
+					var newSplitPowerStat;
 
 					if (!power_stat) {
-						new_power_stat = number_of_cards;
+						newSplitPowerStat = number_of_cards;
 					} else {
-						new_power_stat = power_stat + number_of_cards;
+						newSplitPowerStat = power_stat + number_of_cards;
 					}
 
-					powers.set(power, new_power_stat);
+					powers.set(power, newSplitPowerStat);
 				}
 
 				// Toughnesses
-				const toughness = card_data.rows[0]['toughness'];
+				const toughness = card_data.rows[0].toughness;
 				var toughnesses = stats.get('toughnesses');
 				const toughness_stat = toughnesses.get(toughness);
 				var new_toughness_stat;
@@ -338,18 +336,17 @@ let calc_deck_stats = async() => {
 				toughnesses.set(toughness, new_toughness_stat);
 
 				if (split_card) {
-					const toughness = split_card_data.rows[0]['toughness'];
-					var toughnesses = stats.get('toughnesses');
+					const toughness = split_card_data.rows[0].toughness;
 					const toughness_stat = toughnesses.get(toughness);
-					var new_toughness_stat;
+					var newSplitToughnessStat;
 
 					if (!toughness_stat) {
-						new_toughness_stat = number_of_cards;
+						newSplitToughnessStat = number_of_cards;
 					} else {
-						new_toughness_stat = toughness_stat + number_of_cards;
+						newSplitToughnessStat = toughness_stat + number_of_cards;
 					}
 
-					toughnesses.set(toughness, new_toughness_stat);
+					toughnesses.set(toughness, newSplitToughnessStat);
 				}
 			} else {
 				// Sideboard
@@ -358,12 +355,25 @@ let calc_deck_stats = async() => {
 			}
 		}
 
-		await client.query(insert_deck_stats, [deck_url, stats.get('main'), stats.get('sideboard'), mapToJson(stats.get('layouts')), mapToJson(stats.get('mana_costs')), mapToJson(stats.get('cmcs')), mapToJson(stats.get('colors')), mapToJson(stats.get('supertypes')), mapToJson(stats.get('types')), mapToJson(stats.get('subtypes')), mapToJson(stats.get('powers')), mapToJson(stats.get('toughnesses'))]);
+		await client.query(insert_deck_stats, [
+			deck_url,
+			stats.get('main'),
+			stats.get('sideboard'),
+			mapToJson(stats.get('layouts')),
+			mapToJson(stats.get('mana_costs')),
+			mapToJson(stats.get('cmcs')),
+			mapToJson(stats.get('colors')),
+			mapToJson(stats.get('supertypes')),
+			mapToJson(stats.get('types')),
+			mapToJson(stats.get('subtypes')),
+			mapToJson(stats.get('powers')),
+			mapToJson(stats.get('toughnesses'))
+		]);
 	}
 
 	await client.end();
 	process.exit();
-}
+};
 
 function mapToJson(map) {
 	return JSON.stringify([...map]);
