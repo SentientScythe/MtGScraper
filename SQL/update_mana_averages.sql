2ac1a7dd05e3e80f8d36d2bf29ff341bf5100639
Scott Lamb <sentientscythe@gmail.com>
2021-03-12 17:46:35 -0700

    Add 14 & 16 mana costs

--- SQL/update_mana_averages.sql
+++ SQL/update_mana_averages.sql
@@ -1,9 +1,12 @@
 UPDATE mtg.deck_stats ds
 SET mana_average = CASE
-	WHEN main - land = 0 THEN 0
-	ELSE (("1" + "2" * 2 + "3" * 3 + "4" * 4 + "5" * 5 + "6" * 6 + "7" * 7 + "8" * 8 + "9" * 9 + "10" * 10 + "11" * 11 + "12" * 12 + "13" * 13 + "15" * 15)::DOUBLE PRECISION / (main - land)::DOUBLE PRECISION)
-END
+		WHEN main - land = 0 THEN 0
+		ELSE (
+			(
+				"1" + "2" * 2 + "3" * 3 + "4" * 4 + "5" * 5 + "6" * 6 + "7" * 7 + "8" * 8 + "9" * 9 + "10" * 10 + "11" * 11 + "12" * 12 + "13" * 13 + "14" * 14 + "15" * 15 + "16" * 16
+			)::DOUBLE PRECISION / (main - land)::DOUBLE PRECISION
+		)
+	END
 FROM mtg.cmcs c
-JOIN mtg."types" t
-ON c.deck_url = t.deck_url
-WHERE ds.deck_url = c.deck_url
+	JOIN mtg."types" t ON c.deck_url = t.deck_url
+WHERE ds.deck_url = c.deck_url;
