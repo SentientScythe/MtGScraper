6bbcc465eb8ebe210b7473e7d52c3e6cd0705238
Scott Lamb <sentientscythe@gmail.com>
2021-03-16 15:20:45 -0600

    Fold set_unknown_card_flags.sql into culm.js

--- JS/culm.js
+++ JS/culm.js
@@ -73,6 +73,9 @@ let insertTrueStats = async () => {
 		bar.tick();
 	}
 
+	await client.query(
+		"UPDATE mtg.tournament_decks SET unknown_cards_sb = FALSE, unknown_cards_main = FALSE; UPDATE mtg.tournament_decks SET unknown_cards_sb = TRUE WHERE array_to_string(cards, '') LIKE '%SB%Unknown Card%'; UPDATE mtg.tournament_decks SET unknown_cards_main = TRUE WHERE array_to_string(cards, '') NOT LIKE '%SB%Unknown Card%' AND array_to_string(cards, '') LIKE '%Unknown Card%';"
+	);
 	await client.release();
 	await browser.close();
 	process.exit();
