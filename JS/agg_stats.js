637686f28416c468e92b6be919cb8ba05926a211
Scott Lamb <sentientscythe@gmail.com>
2021-03-16 16:05:40 -0600

    Add delimited stat keys to agg_stats.js

--- JS/agg_stats.js
+++ JS/agg_stats.js
@@ -17,30 +17,53 @@ const ProgressBar = require('progress');
 let aggregateStats = async () => {
 	await client.connect();
 
-	for (const stat of ['layouts', 'cmcs', 'colors', 'supertypes', 'types', 'powers', 'toughnesses']) {
-		await client.query('TRUNCATE TABLE mtg.' + stat + ';');
+	for (const stat of [
+		{ delimit: '', name: 'cmcs' },
+		{ delimit: ',', name: 'colors' },
+		{ delimit: ',', name: 'keywords' },
+		{ delimit: '', name: 'layouts' },
+		{ delimit: '}{', name: 'mana_costs' },
+		{ delimit: '', name: 'powers' },
+		{ delimit: ',', name: 'supertypes' },
+		{ delimit: '', name: 'toughnesses' },
+		{ delimit: ',', name: 'types' }
+	]) {
+		const delimit = stat.delimit;
+		const name = stat.name;
+		await client.query('TRUNCATE TABLE mtg.' + name + ';');
 		const response = await client.query(
 			'SELECT tds.deck_url, ' +
-				stat +
-				' stats FROM mtg.tournament_decks tds LEFT JOIN mtg.deck_stats ds ON tds.deck_url = ds.deck_url WHERE unknown_cards_main = FALSE;'
+				name +
+				' deck_stats FROM mtg.tournament_decks tds LEFT JOIN mtg.deck_stats ds ON tds.deck_url = ds.deck_url WHERE unknown_cards_main = FALSE;'
 		);
-		const responseRows = response.rows;
+		const rows = response.rows;
 		const bar = new ProgressBar(stat + ' progress [:bar] :current/:total :percent :etas', {
-			total: responseRows.length
+			total: rows.length
 		});
 
 		// Gather keys for headers
 		const keys = new Set();
 
-		for (const responseRow of responseRows) {
-			for (const stat of responseRow.stats) {
-				keys.add(stat[0]);
+		for (const row of rows) {
+			for (const deckStat of row.deck_stats) {
+				const deckStatKey = deckStat[0];
+
+				if (delimit) {
+					const deckStatKeys = '}{' === delimit ? deckStatKey.substring(1, deckStatKey.length - 1) : deckStatKey;
+
+					for (const key of deckStatKeys.split(delimit)) {
+						keys.add(key);
+					}
+				} else {
+					keys.add(deckStatKey);
+				}
 			}
 		}
 
+		// Dynamically create the SQL query from the keys
 		const columns = [...keys];
 		columns.unshift('deck_url');
-		var insertStats = 'INSERT INTO mtg.' + stat + ' ("' + columns.join('", "') + '") VALUES (';
+		var insertStats = 'INSERT INTO mtg.' + name + ' ("' + columns.join('", "') + '") VALUES (';
 
 		for (var i = 1; i <= columns.length; i++) {
 			insertStats += 1 === i ? '$' + i : ', $' + i;
@@ -49,23 +72,37 @@ let aggregateStats = async () => {
 		insertStats += ');';
 
 		// Insert stats
-		for (const responseRow of responseRows) {
-			var entry = [responseRow.deck_url];
+		for (const row of rows) {
+			var data = [row.deck_url];
 
 			for (const key of keys.keys()) {
 				var statValue = null;
 
-				for (const stat of responseRow.stats) {
-					if (key === stat[0]) {
-						statValue = stat[1];
-						break;
+				for (const deckStat of row.deck_stats) {
+					const deckStatKeyRaw = deckStat[0];
+
+					if (delimit) {
+						const deckStatKeys =
+							'}{' === delimit ? deckStatKeyRaw.substring(1, deckStatKeyRaw.length - 1) : deckStatKeyRaw;
+
+						for (const deckStatKey of deckStatKeys.split(delimit)) {
+							if (key === deckStatKey) {
+								statValue = deckStat[1];
+								break;
+							}
+						}
+					} else {
+						if (key === deckStatKeyRaw) {
+							statValue = deckStat[1];
+							break;
+						}
 					}
 				}
 
-				entry.push(statValue ? statValue : 0);
+				data.push(statValue ? statValue : 0);
 			}
 
-			await client.query(insertStats, entry);
+			await client.query(insertStats, data);
 			bar.tick();
 		}
 	}
