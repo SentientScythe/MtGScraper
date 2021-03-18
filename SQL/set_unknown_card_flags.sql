59bc08ffe2a2b468f2edd996c1a3acb9f9d23c37
Scott Lamb <sentientscythe@gmail.com>
2021-03-18 09:39:16 -0600

    Terminate SQL statements

--- SQL/set_unknown_card_flags.sql
+++ SQL/set_unknown_card_flags.sql
@@ -1,10 +1,10 @@
 UPDATE mtg.tournament_decks
-SET unknown_cards_sb = FALSE, unknown_cards_main = FALSE
+SET unknown_cards_sb = FALSE, unknown_cards_main = FALSE;
 
 UPDATE mtg.tournament_decks
 SET unknown_cards_sb = TRUE
-WHERE array_to_string(cards, '') LIKE '%SB%Unknown Card%'
+WHERE array_to_string(cards, '') LIKE '%SB%Unknown Card%';
 
 UPDATE mtg.tournament_decks
 SET unknown_cards_main = TRUE
-WHERE array_to_string(cards, '') NOT LIKE '%SB%Unknown Card%' AND array_to_string(cards, '') LIKE '%Unknown Card%'
+WHERE array_to_string(cards, '') NOT LIKE '%SB%Unknown Card%' AND array_to_string(cards, '') LIKE '%Unknown Card%';
