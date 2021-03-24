f8183900cf6ce93cfd48ac6a26957fb5e672821c
Scott Lamb <sentientscythe@gmail.com>
2021-03-23 18:56:01 -0600

    Integrate update_mana_averages

--- JS/agg_stats.js
+++ JS/agg_stats.js
@@ -149,7 +149,11 @@ let aggregateStats = async () => {
 		}
 	}
 
+	await client.query(
+		'UPDATE mtg.deck_stats SET mana_average = CASE WHEN main - land = 0 THEN 0 ELSE (("0.5" * 0.5 + "1" + "2" * 2 + "3" * 3 + "4" * 4 + "5" * 5 + "6" * 6 + "7" * 7 + "8" * 8 + "9" * 9 + "10" * 10 + "11" * 11 + "12" * 12 + "13" * 13 + "14" * 14 + "15" * 15 + "16" * 16 + "1000000" * 1000000)::DOUBLE PRECISION / (main - land)::DOUBLE PRECISION) END FROM mtg.cmcs JOIN mtg."types" ON cmcs.deck_url = "types".deck_url WHERE deck_stats.deck_url = cmcs.deck_url;'
+	);
 	await client.end();
+	process.exit();
 };
 
 aggregateStats();
